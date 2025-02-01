3a83a4fbf9eaf3bbfec5c9a2295d15302f1704e3
Janki23193 <jankirawal93@gmail.com>
2025-02-01 07:55:43 +0530

    Finished working with BlogLayoutTwo file

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import { allBlogs } from 'contentlayer/generated'
 import HomeCoverSection from "../components/Home/HomeCoverSection";
+import FeaturedPosts from "../components/Home/FeaturedPosts";
 export default function Home() {
   console.log(allBlogs)
   return (
      <main className="flex flex-col items-center justify-center">
-       Hare Krishna      
-       <HomeCoverSection blogs={allBlogs
-
-       }/>
+            
+       <HomeCoverSection blogs={allBlogs}/>
+       <FeaturedPosts blogs={allBlogs}/>
      </main>  
     
   );
