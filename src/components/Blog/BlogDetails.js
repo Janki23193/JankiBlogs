74965cd335dfd762d04c5f36b3f49d457a90a723
Janki23193 <jankirawal93@gmail.com>
2025-02-28 03:27:46 +0530

    solved image issue

--- src/components/Blog/BlogDetails.js
+++ src/components/Blog/BlogDetails.js
@@ -10,7 +10,7 @@ const BlogDetails = ({blog, slug: blogSlug}) => {
     justify-around flex-wrap text-lg sm:text-xl font-medium mx-5 md:mx-10 rounded-lg'>
         <time className='m-3'>{format(parseISO(blog.publishedAt), "LLLL d, yyyy")}</time>
         <span className='m-3'>
-          <ViewCounter slug={blogSlug}/>
+          <ViewCounter slug = {blogSlug}/>
         </span>
         <div className='m-3'>
             {blog.readingTime.text}
